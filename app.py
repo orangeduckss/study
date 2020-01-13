57ba1ad7b2c8f904143c03ac1da0d571dbf15393
orangeduckss <orange-duck@naver.com>
2020-01-13 04:36:01 +0000

    app.py

--- app.py
+++ app.py
@@ -1,1 +1,59 @@
-print("Hello world")
+# app.py
+from flask import Flask, request, jsonify
+app = Flask(__name__)
+
+print("helloworld")
+
+@app.route('/getmsg/', methods=['GET'])
+def respond():
+    # Retrieve the name from url parameter
+    """get으로 API 요청이 왔을때 실행  """
+    name = request.args.get("name", None)
+
+    # For debugging
+    print(f"got name {name}")
+
+    response = {}
+
+    # 현재는 parameter name에 대해서만 정의
+    # Check if user sent a name at all
+    if not name:
+        response["ERROR"] = "no name found, please send a name."
+    # Check if the user entered a number not a name
+    elif str(name).isdigit():
+        response["ERROR"] = "name can't be numeric."
+    # Now the user entered a valid name
+    else:
+        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
+
+    # Return the response in json format
+    return jsonify(response)
+
+@app.route('/post/', methods=['POST'])
+def post_something():
+    """post로 API 요청이 왔을때 실행  """
+    param = request.form.get('name')
+    
+    print(param)
+    # 현재는 parameter name에 대해서만 정의
+    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
+    if param:
+        return jsonify({
+            "Message": f"Welcome {name} to our awesome platform!!",
+            # Add this option to distinct the POST request
+            "METHOD" : "POST"
+        })
+    else:
+        return jsonify({
+            "ERROR": "no name found, please send a name."
+        })
+
+# A welcome message to test our server
+@app.route('/')
+    #/ root로 접속시 아래 함수 실행
+def index():
+    return "<h1>Welcome to our server !!</h1>"
+
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
